310173ef9d534d3aa983af7ed5d63279cd8cd867
Gabriel Soares <141067031+gabrielncsoares@users.noreply.github.com>
2023-12-20 00:44:42 -0300

    main.py

--- main.py
+++ main.py
@@ -34,26 +34,25 @@ resultado = apresentar_sorteados(tuplas)
 # print(resultado)
 
 gabriel = [valor for valor in resultado if 'Gabriel Soares' in valor and 'sorteou' in valor and valor.index('Gabriel Soares') < valor.index('sorteou')]
-# joao = {valor for chave, valor in resultado.items() if 'João Silva' in valor and valor.index('João Silva') < valor.index('sorteou')}
-# carol = {valor for chave, valor in resultado.items() if 'Carol Silva' in valor and valor.index('Carol Silva') < valor.index('sorteou')}
-# isabela = {valor for chave, valor in resultado.items() if 'Isabela Lage' in valor and valor.index('Isabela Lage') < valor.index('sorteou')}
-print(gabriel)
+joao = {valor for chave, valor in resultado.items() if 'João Silva' in valor and valor.index('João Silva') < valor.index('sorteou')}
+carol = {valor for chave, valor in resultado.items() if 'Carol Silva' in valor and valor.index('Carol Silva') < valor.index('sorteou')}
+isabela = {valor for chave, valor in resultado.items() if 'Isabela Lage' in valor and valor.index('Isabela Lage') < valor.index('sorteou')}
 
-# def enviar_email():  
-#     corpo_email = f'{list(gabriel)[0].replace('Gabriel Soares', 'Você')}'
-#     msg = email.message.Message()
-#     msg['Subject'] = "Sorteio"
-#     msg['From'] = 'gabrielnicsoares.business@gmail.com'
-#     msg['To'] = 'gabinicsoares@gmail.com'
-#     password = 'cwfn cche qvxu xvei' 
-#     msg.add_header('Content-Type', 'text/html')
-#     msg.set_payload(corpo_email )
+def enviar_email():  
+    corpo_email = f'{list(gabriel)[0].replace('Gabriel Soares', 'Você')}'
+    msg = email.message.Message()
+    msg['Subject'] = "Sorteio"
+    msg['From'] = 'gabrielnicsoares.business@gmail.com'
+    msg['To'] = 'gabinicsoares@gmail.com'
+    password = 'cwfn cche qvxu xvei' 
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(corpo_email )
 
-#     s = smtplib.SMTP('smtp.gmail.com: 587')
-#     s.starttls()
-#     # Login Credentials for sending the mail
-#     s.login(msg['From'], password)
-#     s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
-#     print('Email enviado')
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    # Login Credentials for sending the mail
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    print('Email enviado')
 
-# enviar_email()
+enviar_email()
